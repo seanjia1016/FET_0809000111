6d5ca4a5ee552a7d30ab7eaf454c471a46c789fd
seanjia1016 <digiwin.sean@gmail.com>
2024-11-12 11:53:21 +0800

    clean build

--- WEB-INF/src/connectivity/ws/operations/CIE_RecordService.java
+++ WEB-INF/src/connectivity/ws/operations/CIE_RecordService.java
@@ -306,6 +306,7 @@ public class CIE_RecordService extends com.avaya.sce.runtime.connectivity.ws.Cal
 
 
 
+
     }
 
 }
--- WEB-INF/src/flow/subflow/API_updateVoucherStatus/API_getCashSubscriberInfoByKey.java
+++ WEB-INF/src/flow/subflow/API_updateVoucherStatus/API_getCashSubscriberInfoByKey.java
@@ -1,5 +1,26 @@
 package flow.subflow.API_updateVoucherStatus;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+import org.json.JSONObject;
+
+import com.avaya.sce.runtimecommon.IComplexVariable;
+import com.avaya.sce.runtimecommon.ITraceInfo;
+import com.avaya.sce.runtimecommon.IVariable;
+import com.avaya.sce.runtimecommon.IVariableField;
+import com.crm.utils.Utils;
+import com.infortrends.APIfunction.GetCashSubscriberInfoByKey;
+
+import flow.IProjectVariables;
 /**
  * A basic servlet which allows a user to define their code, generate
  * any output, and to select where to transition to next.
@@ -19,6 +40,8 @@ public class API_getCashSubscriberInfoByKey extends com.avaya.sce.runtime.BasicS
 		super();
 		//}}END:CLASS:CONSTRUCTOR
 	}
+	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.CHINESE);
 
 	/**
 	 * This method allows for custom integration with other Java components.
@@ -39,7 +62,101 @@ public class API_getCashSubscriberInfoByKey extends com.avaya.sce.runtime.BasicS
 	 */
 	public void servletImplementation(com.avaya.sce.runtimecommon.SCESession mySession) {
 
-		// TODO: Add your code here!
+		final String TID = Utils.TID(mySession);
+		
+		String apiName = "getCacheSubscriberInfoByKey";
+
+		final String IP = Utils.IP(mySession);
+		String url = "http://" + IP + "/FET_IVR_MiddleWare/" + apiName;
+		
+		String msisdn = "0989561475";
+		
+		String bpid = Utils.getFieldString(mySession,
+				IProjectVariables.GET_CASH_SUBSCRIBER_INFO_BY_KEY__INPUT,
+				IProjectVariables.GET_CASH_SUBSCRIBER_INFO_BY_KEY__INPUT_FIELD_TRANS_BP_ID);
+		
+		// API開始時間
+				Calendar API_Sdate = Calendar.getInstance();
+				API_Sdate.add(Calendar.SECOND, 0);
+				String API_SDATETIME = sdf.format(API_Sdate.getTime()).toString();
+
+				JSONObject responseJsonObject = null;
+				int responseCode = 0;
+				try {
+					URL	obj = new URL(url);
+					HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+					// add reuqest header
+					con = (HttpURLConnection) obj.openConnection();
+					con.setRequestMethod("POST");
+					con.setRequestProperty("Content-Type", "text/xml;charset=UTF-8");
+
+					// 組成要傳入的JSON物件
+					JSONObject jsonobject = new JSONObject();
+					jsonobject.put("msisdn", msisdn);
+					jsonobject.put("bpid", bpid);
+				
+					String urlParameters = jsonobject.toString();
+					Utils.LogsINFO(mySession, "jsonObject Request(TID:[" + TID + "]" + apiName + "):" + urlParameters);
+					
+					// Send post request
+					con.setDoOutput(true);
+					DataOutputStream wr = null;
+					wr = new DataOutputStream(con.getOutputStream());
+					wr.write(urlParameters.getBytes("UTF-8"));
+					wr.flush();
+					wr.close();
+					responseCode = con.getResponseCode();
+
+					BufferedReader in = null;
+					in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+					String inputLine;
+					StringBuffer response = new StringBuffer();
+					while ((inputLine = in.readLine()) != null) {
+						response.append(inputLine);
+					}
+					in.close();
+					
+					Utils.LogsResponse(mySession, TID, apiName, con, urlParameters, responseCode, response.toString());
+					
+					// print result
+					responseJsonObject = new JSONObject(response.toString());
+					Utils.LogsINFO(mySession, "ResponseJsonObject	:"+responseJsonObject.toString());
+					// API結束時間
+					Calendar API_Edate = Calendar.getInstance();
+					API_Edate.add(Calendar.SECOND, 0);
+					String API_EDATETIME = sdf.format(API_Edate.getTime()).toString();
+					long timelong = API_Edate.getTimeInMillis() - API_Sdate.getTimeInMillis();
+					// API經過時間
+					String APIconnecttimelong = String.valueOf(timelong / 1000);
+					
+					Utils.setFieldString(mySession,
+							IProjectVariables.FE__API__LOG__TBL,
+							IProjectVariables.FE__API__LOG__TBL_FIELD_SEC,
+							APIconnecttimelong);
+
+					if (responseCode == 200) {
+						if (responseJsonObject.has("subscriberLayerInfo.subscriberContext.subscriberId")) {
+							Utils.setFieldString(mySession,
+									IProjectVariables.GET_CASH_SUBSCRIBER_INFO_BY_KEY__OUTPUT,
+									IProjectVariables.GET_CASH_SUBSCRIBER_INFO_BY_KEY__OUTPUT_FIELD_SUBSCRIBER_ID,
+									responseJsonObject.getString("subscriberLayerInfo.subscriberContext.subscriberId"));
+						}
+					}
+				} catch (ArithmeticException ex) {
+					 ex.printStackTrace();
+					 Utils.LogsERROR(mySession, "ArithmeticException Response(TID:["+TID+"]"+apiName+"):" +ex.getMessage());
+				} catch (MalformedURLException e1) {
+					 e1.printStackTrace();
+					 Utils.LogsERROR(mySession, "MalformedURLException Response(TID:["+TID+"]"+apiName+"):" +e1.getMessage());
+				} catch (IOException e) {
+					 e.printStackTrace();
+					 Utils.LogsERROR(mySession, "IOException Response(TID:["+TID+"]"+apiName+"):" +e.getMessage());
+				} catch (Exception e) {
+					 e.printStackTrace();
+					 Utils.LogsERROR(mySession, "Exception Response(TID:["+TID+"]"+apiName+"):" +e.getMessage());
+				} finally {
+					Utils.LogsINFO(mySession, "HttpCode Response:" + responseCode);
+				}
 
 	}
 	/**
@@ -59,14 +176,14 @@ public class API_getCashSubscriberInfoByKey extends com.avaya.sce.runtime.BasicS
 	 * @return a Collection of <code>com.avaya.sce.runtime.Goto</code>
 	 * objects that will be evaluated at runtime.  If there are no gotos
 	 * defined in the Servlet node, then this returns null.
-	 * Last generated by Orchestration Designer at: 2024年11月12日 上午11時12分37秒
+	 * Last generated by Orchestration Designer at: 2024年11月12日 上午11時53分04秒
 	 */
 	public java.util.Collection getBranches(com.avaya.sce.runtimecommon.SCESession mySession) {
 		java.util.List list = null;
 		com.avaya.sce.runtime.Goto aGoto = null;
 		list = new java.util.ArrayList(1);
 
-		aGoto = new com.avaya.sce.runtime.Goto("API_updateVoucherStatus-End", 0, true, "Default");
+		aGoto = new com.avaya.sce.runtime.Goto("API_updateVoucherStatus-API_updateVoucherStatus", 0, true, "Default");
 		list.add(aGoto);
 
 		return list;
