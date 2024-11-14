cba4da266bceac43b666091d951d78724404a31f
seanjia1016 <digiwin.sean@gmail.com>
2024-11-14 10:44:09 +0800

    2024114 - 01 修正OP

--- WEB-INF/src/connectivity/ws/operations/CIE_CreateService.java
+++ WEB-INF/src/connectivity/ws/operations/CIE_CreateService.java
@@ -317,6 +317,7 @@ public class CIE_CreateService extends com.avaya.sce.runtime.connectivity.ws.Cal
 
 
 
+
     }
 
 }
--- WEB-INF/src/connectivity/ws/operations/CIE_RecordService.java
+++ WEB-INF/src/connectivity/ws/operations/CIE_RecordService.java
@@ -320,6 +320,7 @@ public class CIE_RecordService extends com.avaya.sce.runtime.connectivity.ws.Cal
 
 
 
+
     }
 
 }
