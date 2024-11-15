a110cb83fc52c486590c89bc88148327581db458
seanjia1016 <digiwin.sean@gmail.com>
2024-11-15 10:15:28 +0800

    先把DN_556021那邊把noinput nomatch都先拿掉

--- WEB-INF/src/connectivity/ws/operations/CIE_CreateService.java
+++ WEB-INF/src/connectivity/ws/operations/CIE_CreateService.java
@@ -329,6 +329,7 @@ public class CIE_CreateService extends com.avaya.sce.runtime.connectivity.ws.Cal
 
 
 
+
     }
 
 }
--- WEB-INF/src/connectivity/ws/operations/CIE_RecordService.java
+++ WEB-INF/src/connectivity/ws/operations/CIE_RecordService.java
@@ -332,6 +332,7 @@ public class CIE_RecordService extends com.avaya.sce.runtime.connectivity.ws.Cal
 
 
 
+
     }
 
 }
