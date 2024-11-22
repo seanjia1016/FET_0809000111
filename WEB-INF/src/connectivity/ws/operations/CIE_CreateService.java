b70783652631191c64af453bbb0382a4e1145264
seanjia1016 <digiwin.sean@gmail.com>
2024-11-22 21:46:57 +0800

    assign 並組出insert完SQL語法

--- WEB-INF/src/connectivity/ws/operations/CIE_CreateService.java
+++ WEB-INF/src/connectivity/ws/operations/CIE_CreateService.java
@@ -355,6 +355,7 @@ public class CIE_CreateService extends com.avaya.sce.runtime.connectivity.ws.Cal
 
 
 
+
     }
 
 }
--- WEB-INF/src/connectivity/ws/operations/CIE_RecordService.java
+++ WEB-INF/src/connectivity/ws/operations/CIE_RecordService.java
@@ -358,6 +358,7 @@ public class CIE_RecordService extends com.avaya.sce.runtime.connectivity.ws.Cal
 
 
 
+
     }
 
 }
